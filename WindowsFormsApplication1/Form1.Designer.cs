f67915c800fd3940601f6d1fab56f2b4e6a8ad54
MatR <matr095@users.noreply.github.com>
2016-02-29 19:59:07 +0100

    Added files via upload

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Enabled = false;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.button1.Location = new System.Drawing.Point(34, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 116);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Chanter";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button2.Enabled = false;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(34, 242);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(185, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Arrêter de chanter";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button3.Enabled = false;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(34, 283);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(185, 36);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Ecouter";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(34, 79);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(185, 35);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Choisir une musique";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(256, 405);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Opacity = 0.93D;
+            this.Text = "Le Chanteur";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("winmm.dll")]
+        private static extern int mciSendString(string MciComando, string MciRetorno, int MciRetornoLeng, int CallBack);
+        string musica = "";
+        string music1 = "";
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+        }
+        
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog instru = new OpenFileDialog();
+            instru.Filter = "MP3|*.mp3";
+            if (instru.ShowDialog() == DialogResult.OK)
+            {
+                music1 = instru.FileName;
+            }
+            button4.Enabled = false;
+            button1.Enabled = true;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mciSendString("play " + music1, null, 0, 0);
+            mciSendString("open new type waveaudio alias Som", null, 0, 0);
+            mciSendString("record Som", null, 0, 0);
+            button1.Enabled = false;
+            button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mciSendString("stop " + music1, null, 0, 0);
+            mciSendString("pause Som", null, 0, 0);
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "wave|*.wav";
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                mciSendString("save Som " + save.FileName, null, 0, 0);
+                mciSendString("close Som", null, 0, 0);
+            }
+            button2.Enabled = false;
+            button3.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            if (musica == "")
+            {
+                OpenFileDialog open = new OpenFileDialog();
+                open.Filter = "Wave|*.wav";
+                if (open.ShowDialog() == DialogResult.OK) {
+                    musica = open.FileName;
+                }
+            }
+            mciSendString("play " + musica, null, 0, 0);
+        }
+
+        
+    }
+}
